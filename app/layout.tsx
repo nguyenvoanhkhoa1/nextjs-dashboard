e5772c4cfc2adc95c3551dc1dbe9ec218df76772
khoa.nguyen <khoa.nguyen@logixtek.com>
2023-12-04 16:52:57 +0700

    update flow login

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,20 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]/route';
+import SessionProvider from '@/providers/SessionProvider';
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <SessionProvider session={session}>{children}</SessionProvider>
+      </body>
     </html>
   );
 }
